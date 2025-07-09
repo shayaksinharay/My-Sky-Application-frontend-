df2f7495a37bdc6c771adc97f6a84b2bb5135424
Shayak Sinha Ray <shayaksinharay@gmail.com>
2025-07-09 11:51:10 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,30 +1,23 @@
-window.addEventListener('load', function() {
-
-  async function fetchAndUpdateSky() {
-    try {
-      const res = await fetch('https://my-sky-application-backend-7.onrender.com/api/stars');
-      const stars = await res.json();
-      Celestial.display({
-        datapath: "",
-        interactive: true,
-        location: [0,0],
-        stars: stars
-      });
-    } catch (e) {
-      console.error("Failed to load sky data:", e);
-    }
-  }
-
-  fetchAndUpdateSky();
-
+// Define global function so button can call it
+function useMyLocation() {
   if (navigator.geolocation) {
-    document.querySelector('button').addEventListener('click', () => {
-      navigator.geolocation.getCurrentPosition(pos => {
-        Celestial.display({
-          location: [pos.coords.longitude, pos.coords.latitude]
-        });
-      }, () => alert("Could not get location."));
-    });
+    navigator.geolocation.getCurrentPosition(pos => {
+      Celestial.display({
+        location: [pos.coords.longitude, pos.coords.latitude]
+      });
+    }, () => alert("Could not get location."));
+  } else {
+    alert("Geolocation not supported.");
   }
+}
 
+// On page load, initialize default celestial map
+window.addEventListener('load', function() {
+  Celestial.display({
+    width: 800,
+    projection: "aitoff",
+    datapath: "",        // keep empty unless using extra data
+    interactive: true,
+    location: [0, 0]     // default location
+  });
 });
